42a6d45cc900ea61f1f52b8bca82890656388e50
Amy-236 <c6231212@qq.com>
2026-09-14 15:46:07 +0800

    Delete 文件搜索工具 directory

--- 文件搜索工具/src/test/java/TestSearchService.java
+++
@@ -1,9 +0,0 @@
-import service.SearchService;
-
-public class TestSearchService {
-    public static void main(String[] args) {
-        SearchService searchService = new SearchService();
-        searchService.init("D:\\project\\比特科技\\1-教学服务团队\\1-比特课件");
-
-    }
-}
